f843397a1931d06368172a52fc6dd396667ae5d9
Щербина Іван Юрійович <ishcherbyna@priocom.com>
2019-01-18 09:22:13 +0200

    add privacy page

---
+++ wp-content/themes/Talent Alpha/privacy-policy-page.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Template Name:Privacy Policy
+ *
+ */
+?>
+<?php get_header();
+global $mytheme
+?>
+    <div class="wrapper">
+        <section class="banner ">
+            <h1><?php the_title(); ?></h1>
+        </section>
+    </div>
+<section class="wrap-section privacy_policy ">
+    <h2 class='privacy_policy-header'>Talent Alpha Privacy Policy</h2>
+    <div class='privacy_policy-content'>
+        <?php
+        while ( have_posts() ) :
+            the_post();
+
+            the_content();
+
+        endwhile;
+        ?>
+    </div>
+    <hr class="privacy_policy-line">
+</section>
+
+    <div class="wrap-section">
+<?php get_footer(); ?>
